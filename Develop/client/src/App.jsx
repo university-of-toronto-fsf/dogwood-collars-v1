69930ecc5411db1fc5697750aec9c88e7a753ca1
aawllms <aarondwilliams8@gmail.com>
2025-02-05 16:54:49 -0700

    updated image format, added bootstrap to home page

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/header.jsx";
 import Footer from "./components/Footer/footer.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import { Nav } from "./components/Nav/nav.jsx";
 
 function App() {
