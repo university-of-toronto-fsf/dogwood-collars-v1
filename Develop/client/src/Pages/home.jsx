a99f6b7eaffea032f419f9916de8356377a0d43d
aawllms <aarondwilliams8@gmail.com>
2025-02-05 13:48:31 -0700

    added pages and made sure nav bar directs to each page

---
+++ Develop/client/src/Pages/home.jsx
@@ -0,0 +1,24 @@
+function Home() {
+  return (
+    <>
+      <h1>About us</h1>
+      <p>
+        Give your pet the perfect blend of style, comfort and durability with
+        our hand-crafted dog collars. Designed by me with functionality and
+        enjoyment at the forefront of my mind, these collars feature hand-dyed
+        Maple beads and military grade paracord with a 550lb tensile strength-
+        enough for any adventure while staying lightweight and comfortable for
+        movement and every day ware. The slip fit design makes Dogwood Collars
+        easy to put on and take off, and they are fully leash-compatible for
+        walks, training or playtime. The martingale feature of the collar allows
+        it to fit loose and free like a necklace, but when engaged by a leash it
+        cinches up just enough to lose the ability to move over the head. While
+        there are standard sizes available (X-Small- Large), for custom orders
+        it is important to measure your pets head to ensure a perfect fit. See
+        our measurement guide tab prior to ordering.
+      </p>
+    </>
+  );
+}
+
+export default Home;
---
+++ Develop/client/src/components/Footer/footer.jsx
@@ -0,0 +1,26 @@
+function Footer() {
+  const styles = {
+    footerStyle: {
+      position: "absolute",
+      bottom: 0,
+      width: "100%",
+    },
+    divStyle: {
+      background: "black",
+      color: "white",
+    },
+  };
+  return (
+    <>
+      <footer style={styles.footerStyle}>
+        <div className="card text-center" style={styles.divStyle}>
+          <div className="card-body">
+            <p className="card-text">Every collar is handmade</p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
