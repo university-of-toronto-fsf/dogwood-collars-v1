23e64739f946b1a70891dc201ceb25e93587355a
aawllms <aarondwilliams8@gmail.com>
2025-02-05 18:11:00 -0700

    updated home page css and layout. added buy now button to bottom of the page

--- Develop/client/src/components/Nav/nav.jsx
+++ Develop/client/src/components/Nav/nav.jsx
@@ -1,28 +1,31 @@
 function Nav() {
+  const style = {
+    
+  }
   return (
-    <>
+    <div>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
               <a
-                className="nav-link active text-light"
+                className="nav-link active text-dark"
                 aria-current="page"
                 href="/"
               >
                 Home
               </a>
-              <a className="nav-link active text-light" href="/product">
+              <a className="nav-link active text-dark" href="/product">
                 Product
               </a>
-              <a className="nav-link active text-light" href="/cart">
+              <a className="nav-link active text-dark" href="/cart">
                 Cart
               </a>
             </div>
           </div>
         </div>
       </nav>
-    </>
+    </div>
   );
 }
 
