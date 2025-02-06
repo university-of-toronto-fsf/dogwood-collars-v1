dc15eb9184e7f8a901f19560b9140610a755a830
aawllms <aarondwilliams8@gmail.com>
2025-02-06 11:11:24 -0700

    save changes
    pull in CSS style sheet from main so saving changes

--- Develop/client/src/components/Card/card.jsx
+++ Develop/client/src/components/Card/card.jsx
@@ -1,28 +1,24 @@
-import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
-
-function Card() {
+function CardComponent({ collar }) {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+    <div className="card m-5" style={{ width: "18rem" }}>
+      <img
+        src={collar.image}
+        className="card-img-top"
+        alt="..."
+        style={{ width: "100%" }}
+      />
+      <div className="card-body">
+        <h3> {collar.name}</h3>
+        <p className="card-text">{collar.price}</p>
+      </div>
+    </div>
   );
 }
 
-export default Card;
+export default CardComponent;
+
+{
+  /* <a href={collar.name} target="_blank" rel="noopener noreferrer">
+{collar.name}
+</a> */
+}
