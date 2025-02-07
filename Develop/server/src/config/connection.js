5f96ba70c405c8c59d8953f7db6efdd14f124eb3
aawllms <aarondwilliams8@gmail.com>
2025-02-07 14:41:54 -0700

    update to connection ssl

--- Develop/server/src/config/connection.js
+++ Develop/server/src/config/connection.js
@@ -6,7 +6,15 @@ import { Sequelize } from "sequelize";
 // TODO: Create sequelize connection
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
   : new Sequelize(
       process.env.DB_NAME || "",
       process.env.DB_USER || "",
