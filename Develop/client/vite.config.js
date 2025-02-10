9b6f5b1faf3f516422f41bff3a131759499ae8f9
Edward Apostol <ed@edapostol.com>
2025-02-10 14:21:37 -0500

    fix: locked the react front-end to port 3000

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,/* client will be served on this port http://localhost:3000 */
     proxy: {
       "/api": {
         target: "http://localhost:5433",
