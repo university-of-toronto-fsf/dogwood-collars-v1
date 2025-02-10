bfaa2607ff9bc4ae2db98ea954d432e16264e8d7
aawllms <aarondwilliams8@gmail.com>
2025-02-09 20:15:34 -0700

    CSS Updates
    updated css for products page, added hanburger button for sizing of the page

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header/header.jsx";
 import Footer from "./components/Footer/footer.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 // import { Nav } from "./components/Nav/nav.jsx";
 
 function App() {
